4bd9d6336c4e2ce4a648195b39dd09772feab769
Fardus wahid <farduswahid87@gmail.com>
2024-06-10 14:45:18 +0600

    fix

--- src/app/api/auth/[...nextauth]/option.js
+++ src/app/api/auth/[...nextauth]/option.js
@@ -13,7 +13,7 @@ export const option = {
             let userRole = "unpaid";
             const adminEmails = [];
             const preEmails = ["farduswahid87@gmail.com", "atia.afroj5@gmail.com", "najihatabassum9@gmail.com"];
-            const proUsers = ["omorbinfaruk346@gmail.com","shiblerrahman26@gmail. com"];
+            const proUsers = ["omorbinfaruk346@gmail.com","shiblerrahman26@gmail.com"];
             const pmEmails = ["farduswahid31@gmail.com", "najmossakibtanvir@gmail.com"];
 
             if(profile?.email && adminEmails.includes(profile.email)){
