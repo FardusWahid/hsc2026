f403c268651f2b7558c9626f3bc9fc71f4d6bc52
Fardus wahid <farduswahid87@gmail.com>
2024-06-27 15:24:12 +0600

    neofite

--- src/app/api/auth/[...nextauth]/option.js
+++ src/app/api/auth/[...nextauth]/option.js
@@ -12,7 +12,7 @@ export const option = {
 
             let userRole = "unpaid";
             const adminEmails = ["applebyte87@gmail.com","araf55090@gmail.com","tasikurr1@gmail.com","yourmomxsenpai@gmail.com"];
-            const preEmails = ["farduswahid87@gmail.com", "atia.afroj5@gmail.com", "najihatabassum9@gmail.com","labibasrabon109@gmail.com","raiyen1610@gmail.com","muhammad7890twf@gmail.com"];
+            const preEmails = ["farduswahid87@gmail.com", "atia.afroj5@gmail.com", "najihatabassum9@gmail.com","labibasrabon109@gmail.com","raiyen1610@gmail.com","muhammad7890twf@gmail.com","redredding8@gmail.com"];
             const proUsers = ["omorbinfaruk346@gmail.com","shiblerrahman26@gmail.com","samimohammad2008@gmail.com"];
             const pmEmails = ["farduswahid31@gmail.com", "najmossakibtanvir@gmail.com"];
 
