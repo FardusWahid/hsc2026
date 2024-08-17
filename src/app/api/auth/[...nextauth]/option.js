5103cf27eba3c8395dfaf21e2012050055b4d1cf
Fardus wahid <farduswahid87@gmail.com>
2024-08-17 23:56:28 +0600

    newuserfue

--- src/app/api/auth/[...nextauth]/option.js
+++ src/app/api/auth/[...nextauth]/option.js
@@ -12,7 +12,7 @@ export const option = {
 
             let userRole = "unpaid";
             const adminEmails = ["applebyte87@gmail.com","araf55090@gmail.com","tasikurr1@gmail.com","yourmomxsenpai@gmail.com"];
-            const preEmails = ["farduswahid87@gmail.com", "atia.afroj5@gmail.com","labibasrabon109@gmail.com","raiyen1610@gmail.com","study.bd07@gmail.com","motiarrahman910848@gmail.com","abedaassistant@gmail.com","genz.acs2024@gmail.com","aryanabir4422@gmail.com"];
+            const preEmails = ["farduswahid87@gmail.com", "atia.afroj5@gmail.com","labibasrabon109@gmail.com","raiyen1610@gmail.com","study.bd07@gmail.com","motiarrahman910848@gmail.com","abedaassistant@gmail.com","genz.acs2024@gmail.com","aryanabir4422@gmail.com","ctgshop10@gmail.com"];
             const proUsers = ["omorbinfaruk346@gmail.com","shiblerrahman26@gmail.com","samimohammad2008@gmail.com"];
             const pmEmails = ["farduswahid31@gmail.com","najmossakibtanvir@gmail.com","najihatabassum9@gmail.com"];
 
