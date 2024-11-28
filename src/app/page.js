82a88c8b636ea93abe1148a307494a96dab65dd4
FardusWahid <158212453+FardusWahid@users.noreply.github.com>
2024-11-29 04:02:27 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,12 @@ const page = async (prop) => {
   
   return (
     <div className='select-none'>
+
+        <div className='bg-yellow-500 text-gray-900 gap-2 mt-2 flex justify-center items-center'>
+        <h1 className='text-sm font-bold'>Try this new Interface</h1>
+          <a href="https://ipremium.vercel.app" target='_blank' className='underline italic text-sm font-bold'>Click here</a>
+          <p className='text-xs font-bold italic'>[may contain bugs]</p>
+      </div>
      
       <h1 className='chapterHeading md:tracking-tighter'>ACS Academic <span className='max-[446px]:hidden'>Cycles</span> 2026</h1>
 
